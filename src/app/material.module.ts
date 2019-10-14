54210c01e7e443d1a221298e98dfb7a4f159288c
Sayyam <sayyamshah01@gmail.com>
2019-10-14 11:01:29 +0530

    initial commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import {
+    MatButtonModule ,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+
+  } from '@angular/material';
+//import {  } from '@angular/material';
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
+
+})
+export class MaterialModule {}
